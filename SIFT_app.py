64b8a106da0f67d92194ececa4fcc15dff83cfef
SoroushRohanizadeh <rohanisoroush@gmail.com>
2024-10-01 22:06:33 -0700

    homography and sift feature matching working but the reference image is really big when feature matching

--- SIFT_app.py
+++ SIFT_app.py
@@ -5,6 +5,9 @@ from python_qt_binding import loadUi
 
 import cv2
 import sys
+import numpy as np
+
+NUM_MATCHES_FOR_HOMOGRAPHY = 4
 
 class My_App(QtWidgets.QMainWindow):
 
@@ -28,19 +31,19 @@ class My_App(QtWidgets.QMainWindow):
 			self._timer = QtCore.QTimer(self)
 			self._timer.timeout.connect(self.SLOT_query_camera)
 			self._timer.setInterval(1000 / self._cam_fps)
-                        self.sift = cv.SIFT_create()
 
 
 	def SLOT_browse_button(self):
-			dlg = QtWidgets.QFileDialog()
-			dlg.setFileMode(QtWidgets.QFileDialog.ExistingFile)
-			if dlg.exec_():
-				self.template_path = dlg.selectedFiles()[0]
+		dlg = QtWidgets.QFileDialog()
+		dlg.setFileMode(QtWidgets.QFileDialog.ExistingFile)
 
-			pixmap = QtGui.QPixmap(self.template_path)
-			self.template_label.setPixmap(pixmap)
-                        self.goal_keyPoints = self.sift.detectAndCompute(img,None)
-			print("Loaded template image file: " + self.template_path)
+		if dlg.exec_():
+			self.template_path = dlg.selectedFiles()[0]
+
+		pixmap = QtGui.QPixmap(self.template_path)
+		self.template_label.setPixmap(pixmap)
+
+		print("Loaded template image file: " + self.template_path)
 
 	def convert_cv_to_pixmap(self, cv_img):
 		cv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
@@ -51,9 +54,44 @@ class My_App(QtWidgets.QMainWindow):
 		return QtGui.QPixmap.fromImage(q_img)
 
 	def SLOT_query_camera(self):
+		reference = cv2.imread(self.template_path, cv2.IMREAD_GRAYSCALE)
+
 		ret, frame = self._camera_device.read()
-                grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-                grayscale_keyPoints, desc_gray = sift.detectAndCompute(frame, cv2.COLOR_BGR2GRAY)
+			
+		# Features
+		sift = cv2.SIFT_create()
+		kp_image, desc_image = sift.detectAndCompute(reference, None)
+
+		# Feature matching
+		index_params = dict(algorithm=0, trees=5)
+		search_params = dict()
+		flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+		grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		kp_grayframe, desc_grayframe = sift.detectAndCompute(grayframe, None)
+
+		grayframe = cv2.drawKeypoints(grayframe, kp_grayframe, grayframe)
+
+		matches = flann.knnMatch(desc_image, desc_grayframe, k=2)
+
+		good_points = []
+		for m, n in matches:
+			if m.distance < 0.6 * n.distance:
+				good_points.append(m)
+
+		if (len(good_points) <= NUM_MATCHES_FOR_HOMOGRAPHY):
+			frame = cv2.drawMatches(reference, kp_image, grayframe, kp_grayframe, good_points, grayframe)
+		else:
+			query_pts = np.float32([kp_image[m.queryIdx].pt for m in good_points]).reshape(-1, 1, 2)
+			train_pts = np.float32([kp_grayframe[m.trainIdx].pt for m in good_points]).reshape(-1, 1, 2)
+			matrix, mask = cv2.findHomography(query_pts, train_pts, cv2.RANSAC, 5.0)
+			matches_mask = mask.ravel().tolist()
+		
+			# Perspective transform
+			h, w = reference.shape
+			pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+			dst = cv2.perspectiveTransform(pts, matrix)
+			frame = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
 
 		pixmap = self.convert_cv_to_pixmap(frame)
 		self.live_image_label.setPixmap(pixmap)
